caff40719a6f39aff2a1bf7e658087b90f491e7a
masukomi <masukomi@masukomi.org>
2015-03-19 15:38:11 -0400

    ascii_spec should -> expect syntax

--- spec/differ/format/ascii_spec.rb
+++ spec/differ/format/ascii_spec.rb
@@ -3,16 +3,17 @@ require 'spec_helper'
 describe Differ::Format::Ascii do
   it 'should format inserts well' do
     @expected = '{+"SAMPLE"}'
-    Differ::Format::Ascii.call(+'SAMPLE').should == @expected
+    expect(Differ::Format::Ascii.call(+'SAMPLE')).to(eq(@expected))
   end
 
   it 'should format deletes well' do
     @expected = '{-"SAMPLE"}'
-    Differ::Format::Ascii.call(-'SAMPLE').should == @expected
+    expect(Differ::Format::Ascii.call(-'SAMPLE')).to(eq(@expected))
   end
 
   it 'should format changes well' do
-    @expected = '{"THEN" >> "NOW"}'
-    Differ::Format::Ascii.call('THEN' >> 'NOW').should == @expected
+    expect(Differ::Format::Ascii.call(
+      Differ::Change.new({delete: 'THEN', insert: 'NOW'})
+    )).to eq('{"THEN" >> "NOW"}')
   end
 end
